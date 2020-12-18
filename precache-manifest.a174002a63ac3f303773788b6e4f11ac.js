9590566215c227b889812a3fd68cf9fbb1799bf2
tylersanderson <tylersanderson@hotmail.com>
2020-12-17 20:44:50 -0600

    Updates

---
+++ precache-manifest.a174002a63ac3f303773788b6e4f11ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28561cad977480a7abc27afac5ec44b7",
+    "url": "/house-prices-api/index.html"
+  },
+  {
+    "revision": "38f9bf11bca95d0e0f01",
+    "url": "/house-prices-api/static/css/main.d74098ff.chunk.css"
+  },
+  {
+    "revision": "8673b27f24b964a8be54",
+    "url": "/house-prices-api/static/js/2.bf6aeb61.chunk.js"
+  },
+  {
+    "revision": "e032752e73d9dd23de355be73aa79324",
+    "url": "/house-prices-api/static/js/2.bf6aeb61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f9bf11bca95d0e0f01",
+    "url": "/house-prices-api/static/js/main.0ad206cb.chunk.js"
+  },
+  {
+    "revision": "c51cf5937edc14a6b162",
+    "url": "/house-prices-api/static/js/runtime-main.519245f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/house-price-prediction/precache-manifest.dde2bf068bc549d8acbba88029d0eabb.js"
+  "/house-prices-api/precache-manifest.a174002a63ac3f303773788b6e4f11ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/house-price-prediction/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/house-prices-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
